785b05ec9a9e25188208513ee5d5c418a4eba26a
karanDevX11 <bistakaran89@gmail.com>
2025-05-17 17:46:16 +0545

    Add: Learn about normalization, data anomalies in RDBMS and select statement in sql

---
+++ Data-wrangling/SQL/Select_statement.sql
@@ -0,0 +1,12 @@
+select *
+from parks_and_recreation.employee_demographics;
+
+select first_name, 
+last_name, 
+birth_date,
+age,
+(age+10) * 10 + 10    # PEMDAS operation
+from parks_and_recreation.employee_demographics;
+
+select distinct first_name, gender
+from parks_and_recreation.employee_demographics;
